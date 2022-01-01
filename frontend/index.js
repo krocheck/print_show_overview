a4d7cdeff9096f0034439d4a3cf1c9c418eef783
Keith Rocheck <749812+krocheck@users.noreply.github.com>
2022-01-01 01:07:28 -0500

    cleanup

--- frontend/index.js
+++ frontend/index.js
@@ -10,39 +10,22 @@ import {
 import React from 'react';
 import printWithoutElementsWithClass from './print_without_elements_with_class';
 
-const TableName = 'Documentation'
-const ViewName = 'Show Overview'
-
 function PrintRecordsApp() {
-	const base = useBase();
-
-	// We want to render the list of records in this table.
-	const table = base.getTableByName(TableName);
-
-	// The view may have been deleted, so we use getViewByIdIfExists
-	// instead of getViewById. getViewByIdIfExists will return null
-	// if the view doesn't exist.
-	const view = table.getViewByNameIfExists(ViewName);
-
 	return (
 		<div>
-			<Toolbar table={table} />
+			<Toolbar/>
 			<Box margin={3}>
-				<Report view={view} />
+				<Report/>
 			</Box>
 		</div>
 	);
 }
 
-// The toolbar contains the view picker and print button.
-function Toolbar({table}) {
+function Toolbar() {
 	return (
 		<Box className="print-hide" padding={2} borderBottom="thick" display="flex">
 			<Button
 				onClick={() => {
-					// Inject CSS to hide elements with the "print-hide" class name
-					// when the app gets printed. This lets us hide the toolbar from
-					// the print output.
 					printWithoutElementsWithClass('print-hide');
 				}}
 				marginLeft={2}
@@ -53,27 +36,21 @@ function Toolbar({table}) {
 	);
 }
 
-// Renders a <Record> for each of the records in the specified view.
-function Report({view}) {
-	const records = useRecords(view);
-
+function Report() {
 	const base = useBase();
-	const table = base.getTableByName(TableName);
+	const table = base.getTableByName('Documentation');
+	const view = table.getViewByNameIfExists('Show Overview');
 	const field = table.getFieldByName("Notes");
-
-	if (!view) {
-		return <div>Required view (Show Overview) not found!</div>;
-	}
+	const records = useRecords(view);
 
 	return (
 		<div>
 			<Box marginY={3}>
 				<img src="https://trimarq.com/wp-content/uploads/2017/04/New_TriMarq_big.png" width="33%"/>
 				{records.map(record => {
-					// Render a check or an x depending on if the artist is on display or not.
-					const isArtistOnDisplay = record.getCellValue('Include');
+					const isRowIncluded = record.getCellValue('Include');
 					return (
-						(isArtistOnDisplay ? (
+						(isRowIncluded ? (
 							<p>
 								<Text><span style={{fontWeight: 'bold'}}>{record.getCellValue('Name')}</span></Text>
 							<br />
